87703caf7e8ec733c5c3afec06d5e6512ed38fd6
unknown <Daiana Avram>
2020-12-07 14:46:51 -0800

    Problema gasirea celui mai des folosit numar intr-o lista

--- find_most_frequent_number.py
+++ find_most_frequent_number.py
@@ -11,24 +11,24 @@ OUTPUT:
 
 Complexity:
     Time: O(n) = (1 + 1 + 1 + 1 + n + 1 + n^2 + 1) = (6 + 2n^2) = n^2
-    Space:
+    Space: O(n) = n
 """
 
-n = int(input())
-line = input()
+n = int(input())  #1
+line = input()  #1
 
-str_numbers = line.split(' ')
-numbers = []
-for i in range(0, n):
-    numbers.append(int(str_numbers[i]))
+str_numbers = line.split(' ')  #1
+numbers = []  #n
+for i in range(0, n):  #n
+    numbers.append(int(str_numbers[i]))  #1
 
-counter = 0
+counter = 0  #1
 
-for i in str_numbers:
-    number_of_occurences = str_numbers.count(i)
-    if number_of_occurences > counter:
-        counter = number_of_occurences
-        numbers = i
+for i in str_numbers:  #n
+    number_of_occurences = str_numbers.count(i)  #1
+    if number_of_occurences > counter:  #1
+        counter = number_of_occurences  #1
+        numbers = i  #1
 
-print (numbers)
+print (numbers)  #0?
 
